fc0539703c905e8c107371827599e3d9a168932d
Chris Pyles <cpyles@berkeley.edu>
2020-03-02 22:42:24 -0800

    proj01

---
+++ proj/proj01/tests/q1_3.py
@@ -0,0 +1,22 @@
+test = {
+	"name": "q1_3",
+	"points": 1,
+	"suites": [ 
+		{
+			"cases": [ 
+				{
+					"code": r"""
+					>>> len(missing_variables) > 0
+					True
+					""",
+					"hidden": False,
+					"locked": False,
+				}, 
+			],
+			"scored": False,
+			"setup": "",
+			"teardown": "",
+			"type": "doctest"
+		}, 
+	]
+}
